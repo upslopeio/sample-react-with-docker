4158160fd39c759cd7b1c4a7a0259770c5f5a047
Andreas M. Kavountzis <akavount@gmail.com>
2021-05-10 22:04:24 -0600

    initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# Sample React App with Docker
---
+++ src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          This is a sample application that uses Docker.
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
